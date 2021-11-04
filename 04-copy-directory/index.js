0f3255f38b27190cd043f782bd0263a6b7fe361e
ilyapridanov <pkukudmitriykuku@gmail.com>
2021-11-04 22:57:29 +0300

    feat:04-test

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const copyFile = require('fs/promises');
+const readdir = require('fs/promises');
+const rimraf = require('rimraf');
+
+rimraf(path.join(__dirname, 'files-copy'), function () { 
+  fs.mkdir(path.join(__dirname, 'files-copy'),{recursive: true} , err => {
+    if (err) throw err;
+  });
+  
+  const superCopy = async function (folder) {
+    try {
+      await copyFile.copyFile(
+        path.join(__dirname, 'files', folder), 
+        path.join(__dirname, 'files-copy', folder), 
+      );
+    } catch(error) {
+      console.log(error);
+    }
+  };
+  
+  const xxx = async function (folder) {
+    try {
+      const files = await readdir.readdir(folder,{withFileTypes: true});
+      for (const file of files) {
+        if (!file.isDirectory()) {
+          superCopy(file.name);
+        }
+      }
+    } catch (error) {
+      console.error('there was an error:', error.message);
+    }
+  };
+  xxx(path.join(__dirname, 'files'));
+});
+
+
 
