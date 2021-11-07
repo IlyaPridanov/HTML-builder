a99f4b3b30f6d9115a74030326f71107a2284b6d
ilyapridanov <pkukudmitriykuku@gmail.com>
2021-11-07 20:59:56 +0300

    feat:copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,39 +1,59 @@
 const fs = require('fs');
 const path = require('path');
 const copyFile = require('fs/promises');
+const removeFile = require('fs/promises');
 const readdir = require('fs/promises');
-const rimraf = require('rimraf');
 
-rimraf(path.join(__dirname, 'files-copy'), function () { 
-  fs.mkdir(path.join(__dirname, 'files-copy'),{recursive: true} , err => {
-    if (err) throw err;
-  });
-  
-  const superCopy = async function (folder) {
-    try {
-      await copyFile.copyFile(
-        path.join(__dirname, 'files', folder), 
-        path.join(__dirname, 'files-copy', folder), 
-      );
-    } catch(error) {
-      console.log(error);
-    }
-  };
-  
-  const xxx = async function (folder) {
-    try {
-      const files = await readdir.readdir(folder,{withFileTypes: true});
-      for (const file of files) {
+xxx(path.join(__dirname));
+
+fs.mkdir(path.join(__dirname, 'files-copy'),{recursive: true} , err => {
+  if (err) throw err;
+});
+
+async function superCopy (folder) {
+  try {
+    await copyFile.copyFile(
+      path.join(__dirname, 'files', folder), 
+      path.join(__dirname, 'files-copy', folder), 
+    );
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+async function remove () {
+  try {
+    await removeFile.rmdir(
+      path.join(__dirname, 'files-copy'),
+      { recursive: true },
+    );
+    fs.mkdir(path.join(__dirname, 'files-copy'),{recursive: true} , err => {
+      if (err) throw err;
+    });
+    xxx(path.join(__dirname, 'files'));
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+
+async function xxx (folder) {
+  try {
+    const files = await readdir.readdir(folder,{withFileTypes: true});
+    for (const file of files) {
+      if (folder === path.join(__dirname)) {
+        if (file.name === 'files-copy') {
+          remove();
+        }
+      } else if (folder === path.join(__dirname, 'files')) {
         if (!file.isDirectory()) {
           superCopy(file.name);
         }
       }
-    } catch (error) {
-      console.error('there was an error:', error.message);
     }
-  };
-  xxx(path.join(__dirname, 'files'));
-});
 
-
+  } catch (error) {
+    console.error('there was an error:', error.message);
+  }
+}
 
